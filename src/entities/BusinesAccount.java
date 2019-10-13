b6ae607b1fcaa215869a51773c832e87bfd88b7e
Andre Kendji <andre.narita@gmail.com>
2019-10-13 18:31:00 -0300

    Start do projeto, criado classes Accont e businesAccount

---
+++ src/entities/BusinesAccount.java
@@ -0,0 +1,39 @@
+package entities;
+
+public class BusinesAccount extends Account{
+	
+	private Double loanLimit;
+
+	
+	
+	
+	public BusinesAccount() {
+		super();
+	}
+	public BusinesAccount(Integer number, String holder, Double balance, Double loanLimit) {
+		super(number, holder, balance);
+		this.loanLimit = loanLimit;
+	}
+
+
+
+
+
+	public Double getLoanLimit() {
+		return loanLimit;
+	}
+	public void setLoanLimit(Double loanLimit) {
+		this.loanLimit = loanLimit;
+	}
+	
+	
+	public void loanAmount(double amount) {
+		if (amount<= loanLimit) {
+			
+			balance+= amount-10;
+		
+		}
+	}
+	
+
+}
